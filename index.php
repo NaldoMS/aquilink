484304fdeb0c00d97227ca8c058a362716427278
Naldo Maranhão <regisousa7@gmail.com>
2016-07-07 17:08:29 -0300

    Home em desenvolvimento

--- index.php
+++ index.php
@@ -108,8 +108,111 @@
 </div>
 
     <div id="bloco" class="col-lg-12">
+        <div class="col-lg-3" id="testeVelocidade">
+            <p id="acesso">Acesso Rápido</p>
+            <a id="testeVelocidade" href="">
+                <img src="img/wifi.png" id="wifi">
+                <p id="teste">Teste de velocidade</p>
+            </a>
+        </div>
+
+        <img id="divisoria" src="img/divisoria.png" alt="">
+
+        <div class="col-lg-9" id="opcoes">
+
+            <div class="opc" id="assinar">
+                <div id="conteudo">
+                    <img id="assine" src="img/assine2.png" alt="">
+                    <p class="title">Quero Assinar</p>
+                    <p class="texto">Preencha o formulário e entraremos em contato em menos de 24 horas</p>
+                    <a href="assinar"><button type="button" class="btn btn-primary">Assine Já</button></a>
+                </div>
+            </div>
+
+            <div class="opc" id="planos">
+                <div id="conteudo">
+                    <img id="assine" src="img/planos2.png" alt="">
+                    <p class="title">Planos</p>
+                    <p class="texto">Temos planos e preços que cabem no seu bolso. É só escolher e assinar.</p>
+                    <a href="planos"><button type="button" class="btn btn-primary">Ver Planos</button></a>
+                </div>
+            </div>
+
+            <div class="opc" id="onde">
+                <div id="conteudo">
+                    <img id="assine" src="img/cobertura2.png" alt="">
+                    <p class="title">Onde Estamos</p>
+                    <p class="texto">Descubra onde encontrar internet de qualidade, veja onde atendemos.</p>
+                    <a href="cobertura"><button type="button" class="btn btn-primary">Cobertura</button></a>
+                </div>
+            </div>
+
+        </div>
 
     </div>
+
+
+    <div id="bloco2" class="col-lg-12">
+        <div id="areaLink"  class="col-lg-4">
+            <div class="box first">
+                <a class="linkLaranja" href="">
+                    <img src="img/condominios.png" alt="">
+                    <p class="lengenda">Condomínios e Residências</p>
+                </a>
+            </div>
+
+            <div class="box">
+                <a class="linkLaranja" href="">
+                    <img src="img/empresas.png" alt="">
+                    <p class="lengenda">Empresas e Centros Comerciais</p>
+                </a>
+            </div>
+
+            <div class="box last">
+                <a class="linkLaranja" href="">
+                    <img src="img/provedores.png" alt="">
+                    <p class="lengenda">Provedores e Data Centers</p>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div id="bloco3" class="col-lg-12">
+        <div class="col-lg-4 azulF">
+            <div class="conteudo">
+                <img class="imgs" src="img/internetDedicada.png" alt="">
+                <h1>Internet Dedicada</h1>
+                <p class="legenda">Link com internet confiável e garantida para suas necessidades pessoais ou empresariais</p>
+            </div>
+        </div>
+
+        <div class="col-lg-4 azulFr">
+            <div class="conteudo">
+                <img class="imgs" src="img/bandaLarga.png" alt="">
+                <h1>Banda Larga</h1>
+                <p class="legenda">Internet para empresas, residências e condomínios com opções de ultra velocidade. Tenha liberdade para usar a internet como quiser. Nós realmente entregamos a velocidade contratada.</p>
+            </div>
+        </div>
+
+        <div class="col-lg-4 azulF">
+            <div class="conteudo">
+                <img class="imgs" src="img/projetosEspeciais.png" alt="">
+                <h1>Projetos Esoeciais</h1>
+                <p class="legenda">Além dos planos existentes, desenvolvemos a solução que você precisa, de acordo com suas necessidades. Consulte..</p>
+            </div>
+        </div>
+    </div>
+
+    <div id="bloco4" class="col-lg-12">
+        <h1 id="title">O que dizem nossos Clientes</h1>
+        
+        <div class="col-lg-4">
+            <img class="perfil" src="img/6731393.jpg" alt="">
+            <p>“Lorem ipsum dolor sit amet, augue vulputate ullamcorper ac, sagittis nunc tempor. Etiam erat suspendisse quis ante, euismod amet lacus ultricies. Faucibus egestas et ut nulla, egestas massa faucibus, proident quis, euismod ullamcorper wisi urna ac, enim nisl dignissim tellus magna vitae aliquam.”</p>
+        </div>
+        
+    </div>
+
 
     <script>
         (function() {
